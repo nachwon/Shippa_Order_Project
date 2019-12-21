61f697a2c09954f13afb41582fe6fd8d5fc8bbdf
Che1 <nachwon@gmail.com>
2019-12-22 00:26:16 +0900

    <feat>(add_points): Add basic points field

--- shippa_order/users/models.py
+++ shippa_order/users/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users import configs
+
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
+    points = models.PositiveIntegerField(default=configs.DEFAULT_POINTS)
