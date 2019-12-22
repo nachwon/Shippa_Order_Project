a1d2e0f14127f0c713fff8e70f007ece54aa0f7a
Che1 <nachwon@gmail.com>
2019-12-22 13:20:10 +0900

    <feat>(add_points): Add user list create view

--- shippa_order/users/views.py
+++ shippa_order/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserListCreateView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAdminUser]
