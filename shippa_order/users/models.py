28f41bac325cb592d1fa23c126b9e952f910fd45
Che1 <nachwon@gmail.com>
2019-12-23 02:09:34 +0900

    <feat>(add_points_log): Add PointsLog model

--- shippa_order/users/models.py
+++ shippa_order/users/models.py
@@ -7,3 +7,15 @@ from users import configs
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     points = models.PositiveIntegerField(default=configs.DEFAULT_POINTS)
+
+
+class PointsLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points_spent = models.PositiveIntegerField(default=0)
+    points_added = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        operator = "+" if self.points_added else "-"
+        points = self.points_added or self.points_spent
+        return f"{self.user.username} {operator} {points}"
