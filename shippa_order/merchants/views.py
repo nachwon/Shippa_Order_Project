be93375852f34ab2810005df97ff5fa9077ecbf0
cozaro <cozarokk@gmail.com>
2019-12-29 23:17:14 +0900

    <feat>(merchant): added views in 'merchant' app - MerchantListCreateView, MerchantRetrieveUpdateView - MenuListCreateView, MenuRetrieveUpdateView

--- shippa_order/merchants/views.py
+++ shippa_order/merchants/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.db.models import Q
 
-# Create your views here.
+from merchants.models import Merchant, Menu
+from merchants.serializers import MerchantSerializer, MenuSerializer
+
+
+class MerchantListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = MerchantSerializer
+    queryset = Merchant.objects.all()
+
+
+class MerchantRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = MerchantSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Merchant.objects.filter(pk=pk)
+
+
+class MenuListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        merchant_id = self.kwargs['merchant_id']
+        return Menu.objects.filter(merchant=merchant_id)
+
+    def get_serializer_context(self):
+        return {'merchant_id': self.kwargs['merchant_id']}
+
+
+class MenuRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        merchant_id = self.kwargs['merchant_id']
+        pk = self.kwargs['pk']
+        return Menu.objects.filter(Q(merchant=merchant_id) & Q(pk=pk))
+
+    def get_serializer_context(self):
+        return {'merchant_id': self.kwargs['merchant_id']}
