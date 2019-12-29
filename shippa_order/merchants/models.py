2b64fb5573f934ea44ffbe4734eeb2ff37676bdf
cozaro <cozarokk@gmail.com>
2019-12-29 23:15:05 +0900

    <feat>(merchant): added and modified columns of 'merchant'

--- shippa_order/merchants/models.py
+++ shippa_order/merchants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Merchant(models.Model):
     # base
-    id = models.UUIDField(primary_key=True)
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=128)
     email = models.EmailField(max_length=128, unique=True)
     phone = models.CharField(max_length=24, unique=True)
@@ -16,10 +16,10 @@ class Merchant(models.Model):
     # 0 ~ 127(every day)
     # ex> 1010101 AND 0000001(SAT) => 0000001 open!
     # ex> 1010101 AND 0000010(FRI) => 0000000 close!
-    business_day = models.PositiveSmallIntegerField(null=True)
+    business_days = models.PositiveSmallIntegerField(default=0)
 
-    open_time = models.TimeField(null=True)
-    close_time = models.TimeField(null=True)
+    open_time = models.TimeField(default='00:00:00')
+    close_time = models.TimeField(default='00:00:00')
 
     # location
     country_iso = models.CharField(max_length=2)
@@ -30,8 +30,9 @@ class Merchant(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
 
-    indexes = [
-        models.Index(fields=['name', ]),
-        models.Index(fields=['email', ]),
-        models.Index(fields=['phone', ]),
-    ]
+    class Meta:
+        indexes = [
+            models.Index(fields=['name', ]),
+            models.Index(fields=['email', ]),
+            models.Index(fields=['phone', ]),
+        ]
