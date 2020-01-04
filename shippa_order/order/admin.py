7c9a7308c1087715f32384793208ed46fbf818cc
jisoojangMMT <jisoo@mymusictaste.com>
2020-01-04 11:34:58 +0900

    <feat>(comm): Order 모델에 message 필드 추가, 모델어드민 임시 추가.

--- shippa_order/order/admin.py
+++ shippa_order/order/admin.py
@@ -3,10 +3,18 @@ from order.models import Order, OrderItem
 
 
 class OrderAdmin(admin.ModelAdmin):
-    fieldsets = (
+    # list_display = ('user_id', 'status', 'menu_id', 'quantity')
+    # fieldsets = (
+    #     ("user_info", {'fields': ('user_id',)}),
+    #     ("order_status", {'fields': ('status',)}),
+    #     ("order_item_info", {'fields': ('menu_id', 'quantity')})
+    # )
+    pass
 
-    )
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('menu_id', 'quantity')
 
 
 admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderItem)
+admin.site.register(OrderItem, OrderItemAdmin)
