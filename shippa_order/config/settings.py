3ec4a918954bb547f90a2377dfbd66bbb333757a
Che1 <nachwon@gmail.com>
2020-01-04 18:51:01 +0900

    <chore>(che1): Allow all hosts for now

--- shippa_order/config/settings.py
+++ shippa_order/config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = secrets['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if env == 'prod' else True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
