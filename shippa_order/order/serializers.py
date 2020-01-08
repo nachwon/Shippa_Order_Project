094d145bdac70ff44cfe379cd11d6348107ef785
jisoojangMMT <jisoo@mymusictaste.com>
2020-01-08 21:54:19 +0900

    <feat>(comm): add serializers, add view for merchant-admin API

---
+++ shippa_order/order/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    pass
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    pass
--- shippa_order/order/views.py
+++ shippa_order/order/views.py
@@ -2,5 +2,30 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# Merchant API 1. 주문 -> merchant save.
+# Merchant API 1. 주문 상태 변경
+from rest_framework import generics, permissions
 
+from order.serializers import OrderSerializer
+
+
+class UpdateOrderStatusView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        pass
+
+    def patch(self):
+        pass
+
+
+# Merchant API 2. 주문 매출 조회
+class OrderSalesReportView(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        pass
+
+    def get(self):
+        pass
