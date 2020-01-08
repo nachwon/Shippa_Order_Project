b217c586e5d4359235f1385e2b31ed09264c1014
jisoojangMMT <jisoo@mymusictaste.com>
2020-01-08 21:52:42 +0900

    <feat>(comm): add url endpoints for merchant-admin API

--- shippa_order/order/urls.py
+++ shippa_order/order/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from order.views import UpdateOrderStatusView, OrderSalesReportView
+
 urlpatterns = [
+    path('<int:order_id>/status/', UpdateOrderStatusView.as_view()),
+    path('/sales_report/', OrderSalesReportView.as_view())
 ]
