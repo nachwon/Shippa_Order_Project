ad8225ee0bbe3bef48e330e19932436125f99bb5
jisoojangMMT <jisoo@mymusictaste.com>
2020-01-10 00:10:28 +0900

    <fix>(comm): fix endpoints

--- shippa_order/order/urls.py
+++ shippa_order/order/urls.py
@@ -4,10 +4,10 @@ from order.views import OrderListCreateView, OrderRetrieveUpdateDestroyView
 from order.admin_views import UpdateOrderStatusView, OrderSalesReportView, OrderDetailView, OrderListView
 
 urlpatterns = [
-    path('order/<int:order_id>/status/', UpdateOrderStatusView.as_view()),
-    path('order/sales_report/', OrderSalesReportView.as_view()),
-    path('order/self/', OrderListCreateView.as_view()),
+    path('self/', OrderListCreateView.as_view()),
     path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view()),
-    path('order/detail/', OrderDetailView.as_view()),
-    path('order/list/merchant/', OrderListView.as_view())
+    path('admin/detail/', OrderDetailView.as_view()),
+    path('admin/order_list/', OrderListView.as_view()),
+    path('admin/<int:order_id>/status/', UpdateOrderStatusView.as_view()),
+    path('admin/sales_report/', OrderSalesReportView.as_view()),
 ]
