e8d52513f07090c50a29da2317119c9e3ea32fab
woohyun park <woohyun@mymusictaste.com>
2020-01-09 19:49:00 +0900

    <feat>(Order) :add User Order urls

--- shippa_order/order/urls.py
+++ shippa_order/order/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from order.views import OrderListCreateView, OrderRetrieveUpdateDestroyView
 
 urlpatterns = [
+    path('self/', OrderListCreateView.as_view()),
+    path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view())
 ]
