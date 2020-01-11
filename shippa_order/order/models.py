4d30130c21c75dea2577955635261a71a2af94bb
woohyun park <woohyun@mymusictaste.com>
2020-01-11 16:54:24 +0900

    <feat>(Order) :change total_price, menu_price field type decimal to positive integer

--- shippa_order/order/models.py
+++ shippa_order/order/models.py
@@ -21,13 +21,13 @@ class Order(models.Model):
         Refunded = 'REFUNDED', _('Refunded')
 
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    merchant = models.ForeignKey(Merchant, null=True, on_delete=models.PROTECT)
     status = models.CharField(max_length=10, choices=OrderStatus.choices, default=OrderStatus.Pending)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
     message = models.CharField(max_length=30, null=True)
-    merchant_id = models.ForeignKey(Merchant, null=True, on_delete=models.PROTECT)
-    total_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    total_price = models.PositiveIntegerField()
 
 
 class OrderItem(models.Model):
@@ -37,11 +37,11 @@ class OrderItem(models.Model):
             models.Index(fields=['order_id'])
         ]
 
-    order_id = models.ForeignKey(Order, on_delete=models.PROTECT)
-    menu_id = models.ForeignKey(Menu, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
-    menu_price = models.DecimalField(max_digits=12, decimal_places=2)
-    total_price = models.DecimalField(max_digits=12, decimal_places=2)
+    menu_price = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField()
 
     def set_total_price(self, menu_price):
         return menu_price * self.quantity
