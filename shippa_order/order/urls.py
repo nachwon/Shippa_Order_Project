13b4315e05dc69b2ac07235992af810e8f6cfab6
woohyun park <woohyun@mymusictaste.com>
2020-01-27 16:31:05 +0900

    <feat>(Order) :change admin api url order to merchant

--- shippa_order/order/urls.py
+++ shippa_order/order/urls.py
@@ -5,8 +5,5 @@ from order.admin_views import OrderSalesReportView, OrderRetrieveDestroy, OrderL
 
 urlpatterns = [
     path('self/', OrderListCreateView.as_view()),
-    path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view()),
-    path('merchant/<int:merchant_id>/orders/<int:order_id>/', OrderRetrieveDestroy.as_view()),
-    path('merchant/<int:merchant_id>/orders/', OrderListView.as_view()),
-    path('merchant/<int:merchant_id>/sales_report/', OrderSalesReportView.as_view()),
+    path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view())
 ]
