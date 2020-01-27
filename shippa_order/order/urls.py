6e555bcc739151d97338704b0b74a51b8e9d6f17
jisoojangMMT <jisoo@mymusictaste.com>
2020-01-27 17:00:51 +0900

    <fix>(comm): fix admin_view names

--- shippa_order/order/urls.py
+++ shippa_order/order/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from order.views import OrderListCreateView, OrderRetrieveUpdateDestroyView
 
-from order.admin_views import MerchantOrderSalesReportView, MerchantOrderRetrieveDestroy, MerchantOrderListView
-
 urlpatterns = [
     path('self/', OrderListCreateView.as_view()),
-    path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view()),
-    path('merchant/<int:merchant_id>/orders/<int:order_id>/', MerchantOrderRetrieveDestroy.as_view()),
-    path('merchant/<int:merchant_id>/orders/', MerchantOrderListView.as_view()),
-    path('merchant/<int:merchant_id>/sales_report/', MerchantOrderSalesReportView.as_view()),
+    path('<int:id>/', OrderRetrieveUpdateDestroyView.as_view())
 ]
